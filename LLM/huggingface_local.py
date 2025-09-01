b6a846a747b4947bb77dd4036c8e91efb364282a
mkafridi <krieger.85@gmail.com>
2025-09-02 00:24:58 +0500

    Create huggingface_local.py

---
+++ LLM/huggingface_local.py
@@ -0,0 +1,18 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id='meta-llama/Llama-3.2-3B-Instruct',
+    task='text-generation',
+    pipeline_kwargs=dict(
+        temperature=0.5,
+        max_new_tokens=100
+    )
+)
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of Pakistan")
+
+print(result.content)
