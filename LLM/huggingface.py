bcc07eb6db9c83a8f5b2d7c2114c8a1ed777c1af
mkafridi <krieger.85@gmail.com>
2025-09-01 23:02:01 +0500

    Update huggingface.py

--- LLM/huggingface.py
+++ LLM/huggingface.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
-    repo_id="meta-llama/Meta-Llama-3.1-8B-Instruct",
+    repo_id="meta-llama/Llama-3.2-3B-Instruct",
     task="text-generation"
 )
 
